043b592d31fd23625019900900cb7d66608e6ee3
Alex Davis <davisalex100@gmail.com>
2024-07-20 17:08:41 -0500

    Removed auto slide show

--- script-LAPTOP-H886CIJJ-2.js
+++ script-LAPTOP-H886CIJJ-2.js
@@ -6,21 +6,36 @@ function togglemenu() {
 }
 
 /**********PROJECT SLIDES*************** */
-var slideIndex = 0;
-showSlides();
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function showSlides() {
+function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 10000); // Change slide every 10 seconds
 }
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
+
+// Initialize the slides
+showSlides(slideIndex);
+
+// Event listeners for the navigation buttons
+document.querySelector('.prev').addEventListener('click', function() {
+    plusSlides(-1);
+});
+
+document.querySelector('.next').addEventListener('click', function() {
+    plusSlides(1);
+});
